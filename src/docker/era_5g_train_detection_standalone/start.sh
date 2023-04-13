cdc18c33594dfa8f3b91e0a3980d3aa93ec5b25c
Petr Dobeš <idobes@fit.vutbr.cz>
2023-04-13 17:14:33 +0200

    fix: change command in start.sh script for docker to enable sigint

--- src/docker/era_5g_train_detection_standalone/start.sh
+++ src/docker/era_5g_train_detection_standalone/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd
-python3 /root/era_5g_train_detection_standalone/era_5g_train_detection_standalone/interface.py
+exec python3 /root/era_5g_train_detection_standalone/era_5g_train_detection_standalone/interface.py
 
